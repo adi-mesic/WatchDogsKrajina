c2dd12b849752462c820f5ab4c84325695b9c1e1
Ammar Hadzic <ammar.hadzic@gmail.com>
2015-03-13 22:25:55 +0100

    Izbrisan Angular, socket.io dodan

--- index.js
+++ index.js
@@ -34,7 +34,5 @@ server.listen(port)
 
 io.on('connection', function (socket) {
   socket.emit('news', { hello: 'world' })
-  socket.on('my other event', function (data) {
-    console.log(data)
-  })
+  
 })
